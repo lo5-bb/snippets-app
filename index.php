07fe856f014a6fcb9fc9caae501ed6bc45a4ae42
codecalm <codecalm@gmail.com>
2015-03-09 22:46:56 +0100

    demo

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>
+        Snippets
+    </title>
+    <link href='http://fonts.googleapis.com/css?family=Source+Code+Pro:400&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,600,300" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+    <div class="wrap">
+        <header class="header">
+            <h1 class="app-title">V Liceum Ogólnokształcące</h1>
+        </header>
+
+        <div class="app">
+            <main class="main">
+                <div class="code-title">Podgląd kodu</div>
+            </main>
+
+            <aside class="side">
+                <section class="code-block">
+                    <div class="code-title">HTML</div>
+                    <textarea id="code-html"><?= htmlspecialchars(file_get_contents('demo/index.html')) ?></textarea>
+                </section>
+                <section class="code-block">
+                    <div class="code-title">CSS</div>
+                <textarea id="code-css"><?= htmlspecialchars(file_get_contents('demo/style.css')) ?></textarea>
+                </section>
+            </aside>
+        </div>
+    </div>
+    <script type="text/javascript" src="js/codemirror/codemirror.js"></script>
+    <script type="text/javascript" src="js/codemirror/mode/xml/xml.js"></script>
+    <script type="text/javascript" src="js/codemirror/mode/css/css.js"></script>
+    <script type="text/javascript" src="js/codemirror/mode/htmlmixed/htmlmixed.js"></script>
+    <script type="text/javascript" src="js/site.js"></script>
+</body>
+</html>
